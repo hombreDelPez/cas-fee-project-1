653ef135380981aa379cdb47c3bc94c8f0a6e4b8
Manuel Fischer <manuel.fischer@outlook.de>
2021-05-29 19:07:02 +0200

    Implemented the "theme changing" mechanism

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,1 +1,35 @@
-console.log('Hello World');
+// Theme chooser
+const lightThemeValue = 'light-theme';
+const darkThemeValue = 'dark-theme';
+const themeLocalStorageId = 'theme';
+
+const themeChooser = document.querySelector('#choose-theme');
+
+themeChooser?.addEventListener('change', (event) => {
+    handleThemeChangeEvent(event);
+});
+
+function handleThemeChangeEvent(event) {
+    const currentValue = event.target.value;
+    const bodyClassList = document.body.classList;
+
+    if (currentValue === lightThemeValue) {
+        bodyClassList.remove(darkThemeValue);
+    }
+
+    if (currentValue === darkThemeValue) {
+        bodyClassList.add(darkThemeValue);
+    }
+
+    localStorage.setItem(themeLocalStorageId, currentValue);
+}
+
+function initializeTheme() {
+    const savedTheme = localStorage.getItem(themeLocalStorageId);
+    if (savedTheme === darkThemeValue) {
+        document.body.classList.add(darkThemeValue);
+        themeChooser.value = darkThemeValue;
+    }
+}
+
+initializeTheme();
