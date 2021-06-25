65811dfa43d8984bcd35f0180e97ceb2d044b755
Manuel Fischer <manuel.fischer@outlook.de>
2021-06-25 23:46:15 +0200

    Improved the request logging

--- middlewares/common-handler.js
+++ middlewares/common-handler.js
@@ -1,10 +1,12 @@
+import moment from 'moment';
+
 export class CommonHandler {
     doLogging = () => {
         let reqCount = 0;
 
         return (req, res, next) => {
             // eslint-disable-next-line no-console
-            console.log(`#${++reqCount} | ${req.method} | ${req.url}`);
+            console.log(`${moment().format('HH:mm:ss')} | Request #${++reqCount} | ${req.method} | ${req.url}`);
             next();
         };
     }
