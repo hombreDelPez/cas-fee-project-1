13becc459bd2f0ab8c5abfc4867a3879bda40b0b
Manuel Fischer <manuel.fischer@outlook.de>
2021-06-27 22:07:51 +0200

    note-detail.html is now changed according to url

--- controllers/doc-controller.js
+++ controllers/doc-controller.js
@@ -1,4 +1,6 @@
 import path from 'path';
+import fs from 'fs';
+import * as cheerio from 'cheerio';
 
 export class DocController {
     constructor() {
@@ -10,12 +12,33 @@ export class DocController {
         res.sendFile('html/index.html', {root: this.publicRootPath});
     }
 
-    getCreateDocument = async (req, res) => {
-        res.sendFile('html/note-detail.html', {root: this.publicRootPath});
+    getCreateDocument = (req, res) => {
+        fs.readFile(`${this.publicRootPath}html/note-detail.html`, 'utf8',
+            DocController.readFileCallback('Create Note ', 'Create a new Note', req, res));
     }
 
-    getEditDocument = async (req, res) => {
-        res.sendFile('html/note-detail.html', {root: this.publicRootPath});
+    getEditDocument = (req, res) => {
+        fs.readFile(`${this.publicRootPath}html/note-detail.html`, 'utf8',
+            DocController.readFileCallback('Edit Note ', 'Edit an existing Note', req, res));
+    }
+
+    static readFileCallback(titlePrefix, h1Text, req, res) {
+        return (error, data) => {
+            if (error) {
+                throw error;
+            }
+
+            const doc = cheerio.load(data);
+
+            const title = doc('title');
+            title.text(`${titlePrefix}${title.text()}`);
+
+            const h1 = doc('h1');
+            h1.text(h1Text);
+
+            res.set('Content-Type', 'text/html');
+            res.send(doc.html());
+        };
     }
 }
 
